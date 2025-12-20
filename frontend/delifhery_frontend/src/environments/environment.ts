ede2a94102acac76a4c720c2c08d468fab7c9f1a
Mark Deubzer <mark.deubzer@gmail.com>
2025-12-20 17:55:53 +0100

    [Backend] Added docker deployment

--- frontend/delifhery_frontend/src/environments/environment.ts
+++ frontend/delifhery_frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://localhost:7156',
+  apiBaseUrl: 'http://localhost:7156',
   keycloak: {
     url: 'http://localhost:8080',
     realm: 'delifhery',
